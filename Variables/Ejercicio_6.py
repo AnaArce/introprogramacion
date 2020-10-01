28814d4627d475ea99046d77aefbb2412ad7d246
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-01 19:05:22 -0400

    Ejercicio 6 resuelto

---
+++ Variables/Ejercicio_6.py
@@ -0,0 +1,4 @@
+peso = int(input("Ingrese su peso en Kg: "))
+altura = float(input("Ingrese su altura en m: "))
+masa_muscular = peso / altura**2
+print("Tu indice de masa muscular es: ", masa_muscular)
