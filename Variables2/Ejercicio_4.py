e3cb25a83743f516f9957aa581defb634f3c3ae0
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-01 16:52:38 -0400

    EJERCICIOS EN CLASE

---
+++ Variables2/Ejercicio_4.py
@@ -0,0 +1,10 @@
+N1 = int(input("ingresar primer numero: "))
+N2 = int(input("inhresar segundo numero: "))
+E1 = N1 == N2
+E2 = N1 != N2
+E3 = N1 > N2
+E4 = N1 < N2
+print("resultado 1", E1)
+print("resultado 2", E2)
+print("resultado 3", E3)
+print("resultado 4", E4)
