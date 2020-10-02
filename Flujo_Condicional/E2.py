cac7f704f541285cf2d8b7ab373f5d2631b909d5
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-02 16:44:50 -0400

    Añadir ejercicio nuevo

---
+++ Flujo_Condicional/E2.py
@@ -0,0 +1,8 @@
+N1 = int(input("ingrese el primer numero: "))
+N2 = int(input("Ingrese el segundo numero: "))
+if N1 < N2 :
+    print(f"{N1} es menor a {N2}")
+elif N1 == N2 :
+    print(f"{N1} es igual a {N2}")
+else:
+    print(f"{N1} es mayor que {N2}")
