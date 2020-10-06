0603f2489df52bb8cdeaf64ed936a2c8bc540e24
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-06 14:24:03 -0400

    Añadir nuevo ejercico

--- Flujo_Condicional/E4.py
+++ Flujo_Condicional/E4.py
@@ -1,11 +1,9 @@
-W = "Elija un partido: "
-P1 = "MAS"
-P2 = "JUNTOS"
-print(W, P1, P2)
-voto = input("selecccione un partido: ")
-if voto == P1 :
-    print("Usted voto por: MAS ")
-elif voto ==  P2 :
-    print(f"Usted voto por: JUNTOS")
+agua = float(input("Ingrese el consumo de agua en m**3 de la persona: "))
+if agua < 100:
+    print("La persona debe pagar: ", (agua * 1), "Bs")
+elif agua >= 100 and agua <= 499:
+    print("La persona debe pagar: ", (agua * 1.20), "Bs")
+elif agua >= 500 and agua <= 999:
+    print("la persona debe pagar: ", (agua * 1.50), "Bs")
 else:
-    print("Voto invalido")
+    print("la persona debe pagar: ", (agua * 2), "Bs")
