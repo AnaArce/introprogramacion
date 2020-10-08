4c9aba12a032008dad3f83d680dad7fff0ee2efe
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-08 16:41:54 -0400

    Añadir el ejercicio

---
+++ Practico_1/Ejercicio_1.py
@@ -0,0 +1,5 @@
+n = int(input("Ingrese un numero: "))
+if n%2 == 0 :
+    print("Su numero par.")
+else:
+    print("Su numero impar.")
---
+++ Practico_1/Ejercicio_2.py
@@ -0,0 +1,3 @@
+num = int(input("Ingrese un numero: "))
+for i in range(11):
+    print(num, "por", i, "=", num*i)
