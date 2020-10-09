b601a915270d5ab61ea74a2a8f447ea9e8751c66
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-09 16:40:18 -0400

    Añadir cooreccion del ejercicio

--- Practico_1/Ejercicio_5.py
+++ Practico_1/Ejercicio_5.py
@@ -1,10 +1,10 @@
-P1 = input("ingrese una palabra: ")
-P2 = input("Ingrese otra palabra: ")
+P1 = input("Palabra 1: ")
+P2 = input("Palabra 2: ")
 long_P1 = len(P1)
 long_P2 = len(P2)
 if long_P1 > long_P2:
-    print(f"{P1} tiene {long_P1 - long_P2} letras mas que {P2}")
+    print(f"La palabra {P1} tiene {long_P1 - long_P2} letras mas que {P2}")
 elif long_P1 == long_P2:
-    print(f"{P1} tiene el mismo numero de letras que {P2}")
+    print(f"La palabra {P1} tiene el mismo numero de letras que {P2}")
 else:
-    print(f"{P2} tiene {long_P2 - long_P1} letras mas que {P1}")
+    print(f"La palabra {P2} tiene {long_P2 - long_P1} letras mas que {P1}")
