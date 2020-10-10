d8fd5cb4ea5b51e0dd18410300d1c3d383cea66f
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-10 18:06:55 -0400

    Añadir  nuevo ejercicio

---
+++ Practico_1/Ejercicio_9.py
@@ -0,0 +1,21 @@
+print("Operaciones: suma(+), resta(-), multiplicacion(*), divicion(/), potenciacion(**)")
+N1 = int(input("Operando: "))
+N2 = (input("Operador: "))
+N3 = int(input("Operando: "))
+if N2 == "+" :
+    res = N1 + N3
+    print(N1, "+", N3, "=", res)
+elif N2 == "-" :
+    res = N1 - N3
+    print(N1, "-", N3, "=", res)
+elif N2 == "*" :
+    res = N1 * N3
+    print(N1, "*", N3, "=", res)
+elif N2 == "/" :
+    res = N1 / N3
+    print(N1, "/", N3, "=", res)
+elif N2 == "**" :
+    res = N1 ** N3
+    print(N1, "**", N3, "=", res)
+else:
+    print("Operacion invalida")
