4fd898d738b4e7dae43b4742b353071192e28a2b
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-13 16:16:38 -0400

    Añadir correcion

--- Practico_1/Ejercicio_10.py
+++ Practico_1/Ejercicio_10.py
@@ -3,5 +3,9 @@ suma = 0
 while num > 0:
     suma = suma + num
     num = int(input("ingrese la duracion del tramo: "))
-tiempo = suma/60
-print(f"El tiempo total de viaje es: {round(tiempo,2)} horas.")
+operacion1 = suma//60
+operacion2 = suma % 60
+if operacion1 < 10:
+    print(f"Tiempo total de viaje: {operacion1}:{operacion2} horas")
+else:
+    print(f"Tiempo total de viaje: {operacion1}:{operacion2} horas")
