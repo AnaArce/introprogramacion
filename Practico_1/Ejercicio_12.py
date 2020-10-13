8fc00bd8df7f1cd00fb45426e583b64690406ffb
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-13 17:19:46 -0400

    Añadir correcion

--- Practico_1/Ejercicio_12.py
+++ Practico_1/Ejercicio_12.py
@@ -1,21 +1,10 @@
-lista = []
-p = int(input("Ingrese el numero de palabras: "))
-mayor = 0
-menor = 0
-i = 1
-
-while (p > 0):
-    palabra = input("Palabra " + str(i) + ": ")
-    lista.append(palabra)
-    i = i + 1
-    p = p - 1
-print(lista)
-mas_corta = min(lista)
-mas_larga = max(lista)
-for i in range(0,p):
-        if len(mas_corta) < len(p[i]):
-         mas_corta = p[i]
-        elif len(mas_larga) > len(p[i]):
-         mas_larga = p[i]
-print("La palabra mas larga es:", mas_larga, "y la palabra mas corta es:", mas_corta)
-
+n = int(input("Indique cuantas palabras ingresara: "))
+palabra_mas_larga = ""
+palabra_mas_corta = "-1"
+for i in range(1,n+1):
+    palabra_actual = input(f"Ingrese palabras: ")
+    if len(palabra_actual) >= len(palabra_mas_larga):
+        palabra_mas_larga = palabra_actual
+    if len(palabra_actual) <= len(palabra_mas_corta):
+            palabra_mas_corta = palabra_actual
+print(f"La palabra mas larga es {palabra_mas_larga} y la palabra mas corta es {palabra_mas_corta}")
