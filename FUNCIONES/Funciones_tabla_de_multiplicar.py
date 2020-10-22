78a23db00ca1a741e361d2a5a830e9403008357f
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-22 16:18:53 -0400

    Agregar ejercicio

---
+++ FUNCIONES/Funciones_tabla_de_multiplicar.py
@@ -0,0 +1,9 @@
+# Tabla de multiplicar
+def tablam(numi):
+    for i in range(1, 11):
+        print(numi, "x", i, "=", i * numi)
+
+
+for j in range(1,11):
+    print("Tabla de multiplicar de", i, "es: ")
+    tablam(j)
