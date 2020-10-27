94de5728a245d7b58f60268e55b806cc51feb538
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-10-27 17:01:32 -0400

    Añadir

--- FUNCIONES/E2.py
+++ FUNCIONES/E2.py
@@ -6,8 +6,9 @@ def numero_mayor(num1,num2,num3):
         print("El numero mayor es: ",num1)
     elif num2 > num1 and num3 < num2:
         print("El numero mayor es: ",num2)
-    elif num3 > num1 and num2 < num3:
+    else:
         print("El numero mayor es: ",num3)
+    return num1, num2, num3
 
 n1= int(input("Ingrese un numero: "))
 n2= int(input("Ingrese un numero: "))
