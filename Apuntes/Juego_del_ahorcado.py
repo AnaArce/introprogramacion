e06c1d3ed3a20ffd4d74ee53abfc0aad7c671b9c
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-11-17 15:50:17 -0400

    AÑADIR

--- Apuntes/Juego_del_ahorcado.py
+++ Apuntes/Juego_del_ahorcado.py
@@ -1,118 +1,116 @@
-archivo = open("Palabra_Oculta.txt", "r")
-palabrao = archivo.readline()
+def juego(archivo):
+    for palabra in lista:
+        palabra = palabra.replace("\n", "")
+        tam = len(palabra)
+        vectorplb = []
+        vectorguion = []
+        intentos = 6
+        for pos in range(0, tam):
+            vectorplb.append(str(palabra[pos]))
+            vectorguion.append("-")
+        print(vectorguion)
+        while intentos > 0:
+            print("Cantidad de intentos: ", intentos)
+            coincidencias = 0
+            cantguiones = 0
+            letra = input("Ingrese una letra: ")
+            letra = letra.upper()
+            for pos in range(0, tam):
+                if letra == vectorplb[pos]:
+                    vectorguion[pos] = letra
+                    coincidencias = coincidencias + 1
+            print(vectorguion)
+            for pos in range(0, tam):
+                if vectorguion[pos] == "-":
+                    cantguiones = cantguiones + 1
+            if coincidencias == 0:
+                intentos = intentos - 1
+                ahorcado(intentos)
+            if (intentos == 6):
+                print("================")
+                print("=              |")
+                print("=")
+                print("=")
+                print("=")
+                print("=")
+                print("=")
+                print("=")
+                print("====")
+            if (intentos == 5):
+                print("================")
+                print("=              |")
+                print("=              O")
+                print("=")
+                print("=")
+                print("=")
+                print("=")
+                print("=")
+                print("====")
+            if (intentos == 4):
+                print("================")
+                print("=              |")
+                print("=              O")
+                print("=           ---°")
+                print("=")
+                print("=")
+                print("=")
+                print("=")
+                print("====")
+            if (intentos == 3):
+                print("================")
+                print("=              |")
+                print("=              O")
+                print("=           ---°---")
+                print("=")
+                print("=")
+                print("=")
+                print("=")
+                print("====")
+            if (intentos == 2):
+                print("================")
+                print("=              |")
+                print("=              O")
+                print("=           ---°---")
+                print("=              |")
+                print("=              |")
+                print("=")
+                print("=")
+                print("====")
+            if (intentos == 1):
+                print("================")
+                print("=              |")
+                print("=              O")
+                print("=           ---°---")
+                print("=              |")
+                print("=              |")
+                print("=               |")
+                print("=")
+                print("====")
+            if (intentos == 0):
+                print("================")
+                print("=              |")
+                print("=              O")
+                print("=           ---°---")
+                print("=              |")
+                print("=              |")
+                print("=             | |")
+                print("=")
+                print("====")
+            if cantguiones == 0:
+                print("Ganaste, adivinaste la palabra!")
+                intentos = -1
+        if intentos == 0:
+            print("Te quedaste sin intentos, perdiste!")
+archivo= open("palabraoculta.txt","r")
+lista = archivo.readlines()
 archivo.close()
-
-
-def imprimirAhorcado(intentos):
-    if (intentos == 6):
-        print("================")
-        print("=              |")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("====")
-    if (intentos == 5):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("====")
-    if (intentos == 4):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=           ---°")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("====")
-    if (intentos == 3):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=           ---°---")
-        print("=")
-        print("=")
-        print("=")
-        print("=")
-        print("====")
-    if (intentos == 2):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=           ---°---")
-        print("=              |")
-        print("=              |")
-        print("=")
-        print("=")
-        print("====")
-    if (intentos == 1):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=           ---°---")
-        print("=              |")
-        print("=              |")
-        print("=               |")
-        print("=")
-        print("====")
-    if (intentos == 0):
-        print("================")
-        print("=              |")
-        print("=              O")
-        print("=           ---°---")
-        print("=              |")
-        print("=              |")
-        print("=             | |")
-        print("=")
-        print("====")
-
-
-palabraoculta = palabrao
-tam = len(palabraoculta)
-vectorPalabraOculta = []
-vectorPalabraGuiones = []
-intentos = 6
-
-for pos in range(0, tam, 1):
-    vectorPalabraOculta.append(str(palabraoculta[pos]))
-    vectorPalabraGuiones.append("-")
-
-print(vectorPalabraGuiones)
-while intentos > 0:
-    fallas = 0
-
-    print("Cantidad de intentos: ", intentos)
-    coincidencias = 0
-    cantidadguiones = 0
-    letra = input("Ingrese una letra: ")
-    lt=letra.upper()
-    for pos in range(0, tam, 1):
-        if (lt == vectorPalabraOculta[pos]):
-            vectorPalabraGuiones[pos] = lt
-            coincidencias = coincidencias + 1
-
-    print(vectorPalabraGuiones)
-
-    for pos in range(0, tam, 1):
-        if (vectorPalabraGuiones[pos] == "-"):
-            cantidadguiones = cantidadguiones + 1
-
-    if (coincidencias == 0):
-        intentos = intentos - 1
-
-    if (cantidadguiones == 0):
-        print("Ganaste, adivinaste la palabra")
-        intentos = -1
-
-if (intentos == 0):
-    print("Se terminaron tus intentos! Perdiste!")
+juego(archivo)
+archivo = open("palabraoculta2", "r")
+lista = archivo.readlines()
+archivo.close()
+juego(archivo)
+archivo = open("palabraoculta3", "r")
+lista = archivo.readlines()
+archivo.close()
+juego(archivo)
 
