4328df04f905daf89e1d23de57bc1625fc79ff07
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-11-26 17:08:45 -0400

    Añadir ejercicico

--- Practico/Ejercicio_1.py
+++ Practico/Ejercicio_1.py
@@ -1,12 +1,16 @@
 #Diseñar un programa que almacene en  un vector llamado FACT, el factorial de los primeros 20 números naturales. FACT = {1!, 2!, 3!, … 20!}
-#Debe diseñar una función que calcule el factorial de un número, por lo tanto, esta función tiene un parámetro y DEVUELVE (o retorna) un valor.
+#Debe diseñar una función que calcule el factorial de un número, por lo tanto, esta función tiene un parámetro y DEVUELVE (o retorna) un valor
+num = int(input("Ingrese el numero al que quiera sacar factorial: "))
+fact = []
+for i in range(1,20+1):
+    numr= str(i) + "!"
+    fact.apped(numr)
+print(fact)
 
-num_f=1
-FACT=[]
-for num in range(1,21):
-    num_f = num*num_f
-    FACT.append(num_f)
-
-print(FACT)
+mul =1
+while num > 1:
+    mul= num * mul
+    num = num-1
+    print("El factorial de ", num, "es:",mul)
 
 
