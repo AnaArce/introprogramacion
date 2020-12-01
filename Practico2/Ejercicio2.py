50105baa7ac366b9cc1ba1f93d9c5fcb0890c21d
DOCENTES <ana.arce.f@ucb.edu.bo>
2020-12-01 17:02:28 -0400

    Añadir Ejercicios

---
+++ Practico2/Ejercicio2.py
@@ -0,0 +1,34 @@
+vi=int(input("Numero inicial: "))
+vf=int(input("Numero final: "))
+
+valores=list(range(vi,vf-1,-1))
+print(valores)
+
+def pares (numeros):
+    pares= []
+    for n in numeros:
+        if n % 2 == 0:
+            pares.append(n)
+    return pares
+
+valores2= pares(valores)
+print(valores2)
+
+def sumar (numeros):
+    suma =0
+    for n in numeros:
+        suma+= n
+    return suma
+
+print(sumar(valores2))
+
+def impares (numeros):
+    impares=[]
+    for n in numeros:
+        if n %2 !=0:
+            impares.append(n)
+    return impares
+
+valores3=impares(valores)
+print(valores3)
+print(sumar(valores3))
